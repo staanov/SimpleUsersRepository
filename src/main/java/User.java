a722cae1705994eaa864cb62e916c12587d2732a
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-02 21:13:12 +0300

    The initial commit. Maven project was created. User and UserRepository classes were added

---
+++ src/main/java/User.java
@@ -0,0 +1,51 @@
+public class User {
+    private String login;
+    private String password;
+    private String firstName;
+    private String secondName;
+
+    public User(String login, String password, String firstName, String secondName) {
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/UserRepository.java
@@ -0,0 +1,28 @@
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+public class UserRepository implements Observable {
+    public void createUser(User user) {
+
+    }
+
+    public User getUser(String login) {
+        return null;
+    }
+
+    public void updateUser(User user) {
+
+    }
+
+    public void deleteUser(User user) {
+
+    }
+
+    public void addListener(InvalidationListener invalidationListener) {
+
+    }
+
+    public void removeListener(InvalidationListener invalidationListener) {
+
+    }
+}
