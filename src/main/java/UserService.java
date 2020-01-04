a19a67ef006b63320eeb02262c6d0f429e94d06c
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-04 19:41:31 +0300

    UserService class was added

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,7 +1,46 @@
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
+import java.util.List;
 
-import java.util.Set;
+public class UserService {
+    private static UserDao userDao;
 
-public class UserService  {
+    public UserService() {
+        userDao = new UserDao();
+    }
+
+    public User getUser(String login) {
+        userDao.openCurrentSession();
+        User user = userDao.getUser(login);
+        userDao.closeCurrentSession();
+        return user;
+    }
+
+    public List<User> getAllUsers() {
+        userDao.openCurrentSession();
+        List<User> users = userDao.getAllUsers();
+        userDao.closeCurrentSession();
+        return users;
+    }
+
+    public void insertUser(User user) {
+        userDao.openCurrentSessionWithTransaction();
+        userDao.insertUser(user);
+        userDao.closeCurrentSessionWithTransaction();
+    }
+
+    public void updateUser(User user) {
+        userDao.openCurrentSessionWithTransaction();
+        userDao.updateUser(user);
+        userDao.closeCurrentSessionWithTransaction();
+    }
+
+    public void deleteUser(String login) {
+        userDao.openCurrentSessionWithTransaction();
+        User user = userDao.getUser(login);
+        userDao.deleteUser(user);
+        userDao.closeCurrentSessionWithTransaction();
+    }
+
+    public UserDao userDao() {
+        return userDao;
+    }
 }
