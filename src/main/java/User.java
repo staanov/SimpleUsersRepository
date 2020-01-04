6f6d4c11348adff56182bc96fdc890d1c61bedbb
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-04 12:01:12 +0300

    User entity design using Hibernate annotations

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,7 +1,19 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "userrepo")
 public class User {
+    @Id
+    @Column(name = "login")
     private String login;
+    
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String secondName;
 
     public User() {
