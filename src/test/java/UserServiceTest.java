6618830b0fc20326c0b07a614c30cc5628b81f03
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-07 14:34:49 +0300

    Unit tests: better names for test methods and some negative tests were added

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.persistence.PersistenceException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,15 +22,21 @@ class UserServiceTest {
     }
 
     @Test
-    void getUser() {
+    void getExistingUserShouldReturnUser() {
         User testUser = new User("Antonio", "fk399kKS", "Anton", "Ivanov");
         userService.insertUser(testUser);
-        assertEquals(new User("Antonio", "fk399kKS", "Anton", "Ivanov"),
-                userService.getUser("Antonio"));
+        Optional<User> extractedUser = Optional.ofNullable(userService.getUser("Antonio"));
+        assertTrue(extractedUser.isPresent());
+        assertEquals(testUser, extractedUser.get());
     }
 
     @Test
-    void getAllUsers() {
+    void getUserWhoDoesNotExistShouldReturnEmptyRow() {
+        assertFalse(Optional.ofNullable(userService.getUser("Antonio")).isPresent());
+    }
+
+    @Test
+    void getAllUsersShouldReturnListOfThreeInsertedUsers() {
         User user1 = new User("Homyak", "jeijDW29", "Petr", "Khomyakov");
         User user2 = new User("Antonio", "fk399kKS", "Anton", "Ivanov");
         User user3 = new User("Ohotnitsa", "KFWJK202kf2", "Larisa", "Petrova");
@@ -39,17 +46,37 @@ class UserServiceTest {
         List<User> allUsers = userService.getAllUsers();
         assertEquals(3, allUsers.size());
         assertTrue(allUsers.stream().anyMatch(user->user.getLogin().equals("Homyak")));
+        assertTrue(allUsers.stream().anyMatch(user->user.getLogin().equals("Antonio")));
+        assertTrue(allUsers.stream().anyMatch(user->user.getLogin().equals("Ohotnitsa")));
     }
 
     @Test
-    void insertUser() {
+    void insertUserShouldInsertRowInDatabase() {
         User testUser = new User("Antonio", "fk399kKS", "Anton", "Ivanov");
         userService.insertUser(testUser);
         assertEquals(List.of(testUser), userService.getAllUsers());
     }
 
     @Test
-    void updateUser() {
+    void updatePasswordOfUserShouldSucceed() {
+        User testUser = new User("Antonio", "fk399kKS", "Anton", "Ivanov");
+        userService.insertUser(testUser);
+        testUser.setPassword("fji3FWk");
+        userService.updateUser(testUser);
+        assertEquals("fji3FWk", userService.getUser("Antonio").getPassword());
+    }
+
+    @Test
+    void updateFirstNameOfUserShouldSucceed() {
+        User testUser = new User("Antonio", "fk399kKS", "Anton", "Ivanov");
+        userService.insertUser(testUser);
+        testUser.setFirstName("Ivan");
+        userService.updateUser(testUser);
+        assertEquals("Ivan", userService.getUser("Antonio").getFirstName());
+    }
+
+    @Test
+    void updateLastNameOfUserShouldSucceed() {
         User testUser = new User("Antonio", "fk399kKS", "Anton", "Ivanov");
         userService.insertUser(testUser);
         testUser.setLastName("Petrov");
@@ -58,11 +85,18 @@ class UserServiceTest {
     }
 
     @Test
-    void deleteUser() {
+    void deleteUserShouldDeleteUserFromDatabase() {
         User testUser = new User("Antonio", "fk399kKS", "Anton", "Ivanov");
         userService.insertUser(testUser);
         userService.deleteUser(testUser.getLogin());
         Optional<User> optionalUser = Optional.ofNullable(userService.getUser(testUser.getLogin()));
         assertFalse(optionalUser.isPresent());
+    }
+
+    @Test
+    void insertOfExistingUserShouldBePrevented() {
+        User testUser = new User("Antonio", "fk399kKS", "Anton", "Ivanov");
+        userService.insertUser(testUser);
+        assertThrows(PersistenceException.class, () -> userService.insertUser(testUser));
     }
 }
