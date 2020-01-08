1321644b58734c96acee2572857dda7aacc5f7db
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-08 19:34:08 +0300

    TestHelper class and script for creating a table were added

---
+++ src/main/resources/scripts/create_users.sql
@@ -0,0 +1,6 @@
+create table users(
+     login varchar(100) NOT NULL PRIMARY KEY,
+     password varchar(100) NOT NULL,
+     first_name VARCHAR(100) NOT NULL,
+     last_name VARCHAR(100) NOT NULL
+);
