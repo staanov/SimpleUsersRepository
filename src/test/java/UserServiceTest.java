f7dd2b419d2754ec46edc16ea75d5930c9b954f6
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-01-08 20:05:28 +0300

    The attempt to substitute real db on embedded db in tests

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,7 +1,16 @@
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.PersistenceException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,12 +19,52 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceTest {
 
     private static UserService userService = new UserService();
+    private static EmbeddedPostgres embeddedPostgres = null;
+    private static SessionFactory sessionFactory = createSessionFactory();
+
+    @BeforeAll
+    public static void setUpDb() {
+        try {
+            embeddedPostgres = EmbeddedPostgres.builder()
+                    .setPort(5433)
+                    .start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        userService.getUserDao().setCurrentSessionFactory(sessionFactory);
+
+        if (embeddedPostgres != null) {
+            TestHelper.executeScript(embeddedPostgres.getPostgresDatabase(), "create_users.sql");
+        }
+    }
+
+    @AfterAll
+    public static void shutdownDb() {
+        try {
+            embeddedPostgres.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static SessionFactory createSessionFactory() {
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .loadProperties("hibernate.properties")
+                .build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(User.class)
+                .buildMetadata();
+
+        return metadata.buildSessionFactory();
+    }
 
     @BeforeEach
     void deleteAllUsers() {
         List<User> allUsers = userService.getAllUsers();
         if (allUsers.size() != 0) {
-            for (User user: allUsers) {
+            for (User user : allUsers) {
                 userService.deleteUser(user.getLogin());
             }
         }
@@ -45,9 +94,9 @@ class UserServiceTest {
         userService.insertUser(user3);
         List<User> allUsers = userService.getAllUsers();
         assertEquals(3, allUsers.size());
-        assertTrue(allUsers.stream().anyMatch(user->user.getLogin().equals("Homyak")));
-        assertTrue(allUsers.stream().anyMatch(user->user.getLogin().equals("Antonio")));
-        assertTrue(allUsers.stream().anyMatch(user->user.getLogin().equals("Ohotnitsa")));
+        assertTrue(allUsers.stream().anyMatch(user -> user.getLogin().equals("Homyak")));
+        assertTrue(allUsers.stream().anyMatch(user -> user.getLogin().equals("Antonio")));
+        assertTrue(allUsers.stream().anyMatch(user -> user.getLogin().equals("Ohotnitsa")));
     }
 
     @Test
